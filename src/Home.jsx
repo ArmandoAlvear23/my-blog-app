009d4902a65badb144551db2ad51f9ddd223a4f4
Armando Alvear <armando.alvear01@outlook.com>
2022-11-17 00:57:31 -0600

    added temp buttons

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, mondo');
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`Hello, ${name}`);
+    }
+
     return (
       <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={() => handleClick2('person viewing my github')}>Click me 2.0</button>
       </div>
     );
 }
