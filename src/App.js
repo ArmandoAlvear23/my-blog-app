82c89741ca495440b2a173d42e0a4db055b6e7b8
Armando Alvear <armando.alvear01@outlook.com>
2022-11-17 00:28:03 -0600

    added home component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-  const title = "Welcome to my new blog";
-  const likes = 50;
-  const person = { name: "mondo", age: 30 };
-
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{person.name}</p>
+        <Home />
       </div>
     </div>
   );
