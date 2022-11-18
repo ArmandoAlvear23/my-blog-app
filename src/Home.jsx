1cdc91c0376ccc8d8a8c7685458dd6c7b5adfee2
Armando Alvear <armando.alvear01@outlook.com>
2022-11-17 22:52:08 -0600

    added blog preview

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,20 @@
 import {useState} from 'react'
 
 const Home = () => {
-
-    const [name, setName] = useState('Epona');
-    const [age, setAge] = useState(7);
-
-    const handleClick = () => {
-        if(name === 'Epona')
-        {
-            setName('Zelda');
-            setAge(25);
-        } else {
-            setName('Epona');
-            setAge(8);
-        }
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'Forest Adventures', body: 'lorem ipsum...', author: 'Link', id: 1 },
+        { title: 'Ranch Activites', body: 'lorem ipsum...', author: 'Epona', id: 2 },
+        { title: 'Castle Diaries', body: 'lorem ipsum...', author: 'Zelda', id: 3 },
+    ]);
     
     return (
       <div className="home">
-        <h2>Homepage</h2>
-        <p>{name} is {age} years old</p>
-        <button onClick={handleClick}>Click me</button>
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2>{ blog.title }</h2>
+                <p>{ blog.author }</p>
+            </div>
+        ))}
       </div>
     );
 }
